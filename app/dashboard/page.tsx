269752c18ea8cc93ed7b896fb9750cd4a48b9ce2
ziad <zyadd.aymann@gmail.com>
2025-05-02 17:35:41 +0300

    added question mark to ensure if the user undefined

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,47 +1,44 @@
 import { buttonVariants } from "@/components/ui/button";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import Link from "next/link";
 import { prisma } from "../utils/db";
-import BlogPostCard from "@/components/BlogPostCard";
-import { redirect } from "next/navigation";
-import { BlogPost } from "@prisma/client";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import  BlogPostCard  from "@/components/BlogPostCard";
 
-async function getData(userId: string): Promise<BlogPost[]> {
-    const data = await prisma.blogPost.findMany({
-        where: {
-            authorId: userId
-        },
-        orderBy: {
-            createdAt: "desc"
-        },
-    })
-    return data;
+async function getData(userId: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = await prisma.blogPost.findMany({
+    where: {
+      authorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
 }
 
-export default async function DashboardRoute(){
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+export default async function DashboardRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || !user.id) {
-        return redirect('api/auth/register');
-    }
+  const data = await getData(user?.id);
 
-    const data = await getData(user.id); 
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
 
-    return (
-        <div className="flex flex-col justify-center pt-1.5">
-            <div>hi <span className="font-bold text-center">{user.given_name}</span> from dashboard</div>
-            <div className="flex justify-between items-center my-2">
-                <h1 className="text-3xl font-medium">Your Blog Articles</h1>
-                <Link href="/dashboard/create" className={buttonVariants()}>Create Post</Link>
-            </div>
+        <Link className={buttonVariants()} href="/dashboard/create">
+          Create Post
+        </Link>
+      </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {data.map((item) => (
-                    <BlogPostCard data={item} key={item.id} />
-                ))}
-            </div>
-        </div>
-    )
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <BlogPostCard data={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
